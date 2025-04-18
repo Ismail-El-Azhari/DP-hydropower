6fb082693ea1ebbe58325281b91141cc49459c33
Ismail El Azhari <95006185+Ismail-El-Azhari@users.noreply.github.com>
2025-04-17 20:25:18 -0400

    Removed Copulas+Gibbs
    Removed Copulas+Gibbs

---
+++ runoff/paper_transitions.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+matrices= {
+  "6_7":np.array([]),
+  "7_8":np.array([]),
+  "8_9":np.array([]),
+  "9_10":np.array([]),
+  "10_11":np.array([]),
+  "11_12":np.array([]),
+  "12_1":np.array([]),
+  "1_2":np.array([]),
+  "2_3":np.array([]),
+  "3_4":np.array([]),
+  "4_5":np.array([]),
+  "5_6":np.array([])
+}
