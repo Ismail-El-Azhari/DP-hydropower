331869eff4aa3d9586319b6972a3404b5c3a0a1d
Ismail El Azhari <95006185+Ismail-El-Azhari@users.noreply.github.com>
2025-04-20 17:28:15 -0400

    Discretized water levels
    I discretized water levels and release levels + Removed the old transitions file

--- SDP/bellman.py
+++ SDP/bellman.py
@@ -1,2 +1,12 @@
-from SDP.utils import power
 
+import numpy as np
+from SDP.utils import power,ZB_LEVELS
+
+months = [
+    "June", "July", "August", "September", "October", "November",
+    "December", "January", "February", "March", "April", "May"
+]
+
+# Initialize value and policy tables
+J = {month: np.zeros(len(ZB_LEVELS)) for month in months}
+policy = {month: np.zeros(len(ZB_LEVELS)) for month in months}
--- SDP/utils.py
+++ SDP/utils.py
@@ -1,2 +1,16 @@
 def power(runoff, zb, ze, efficiency=0.9):
   return (runoff + zb - ze) * efficiency
+
+import numpy as np
+
+# Water levels in meters (states)
+Z_MIN = 740
+Z_MAX = 830
+Z_STEP = 1
+ZB_LEVELS = np.arange(Z_MIN, Z_MAX + 1, Z_STEP)
+
+# Release levels in meters (actions)
+RELEASE_MIN = 0
+RELEASE_MAX = 50
+RELEASE_STEP = 1
+RELEASES = np.arange(RELEASE_MIN, RELEASE_MAX + 1, RELEASE_STEP)
